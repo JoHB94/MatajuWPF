ef1b32b1ca0d887c97f43b9de0279b8d9debc8fc
JoHB94 <webdp1503@gmail.com>
2024-12-12 17:33:32 +0900

    LoginViewModel Http 메서드 작성

--- VMFolder/LoginViewModel.cs
+++ VMFolder/LoginViewModel.cs
@@ -1,13 +1,44 @@
-﻿using System;
+﻿using Mataju.ModelFolder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Mataju.VMFolder
 {
     internal class LoginViewModel : ViewModelBase
     {
+        private LoginModel _loginModel = new LoginModel();
 
+        public LoginModel LoginModel 
+        {
+            get => _loginModel;
+            set
+            {
+                if (_loginModel != value)
+                {
+                    _loginModel = value;
+                    OnPropertyChanged(nameof(LoginModel));
+                }
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            //LoginCommand = new RelayCommand(async() => await )
+        }
+
+        public async Task LoginAsync()
+        {
+
+            //API 엔드포인트
+            string apiUrl = "";
+        }
     }
 }
