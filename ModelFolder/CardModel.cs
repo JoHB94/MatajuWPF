456da85703a2205f31701c3a444b846ba06ad15f
JoHB94 <webdp1503@gmail.com>
2024-12-16 16:52:37 +0900

    add Resources 사진,CardModel/refactoring getHouse()

---
+++ ModelFolder/CardModel.cs
@@ -0,0 +1,19 @@
+﻿using Mataju.VMFolder;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mataju.ModelFolder
+{
+    internal class CardModel : ViewModelBase
+    {
+        public int HouseId { get; set; }
+        public string HouseAdd { get; set; }
+        public string Province { get; set; }
+        public string ImgPath { get; set; }
+
+    }
+}
