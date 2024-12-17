5919d5465230cfac60a8b4defcc5910974351772
JoHB94 <webdp1503@gmail.com>
2024-12-17 20:00:18 +0900

    add BookingModel 추가

---
+++ ModelFolder/BookingModel.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mataju.ModelFolder
+{
+    internal class BookingModel
+    {
+        [JsonProperty("userId")]
+        public int UserId {  get; set; }
+
+        [JsonProperty("houseId")]
+        public int HouseId { get; set; }
+
+        [JsonProperty("unitSize")]
+        public string UnitSize { get; set; }
+
+        [JsonProperty("startDate")]
+        public string StartDate { get; set; }
+
+        [JsonProperty("durationDays")]
+        public string DurationDays { get; set; }
+
+        [JsonProperty("userNote")]
+        public string UserNote { get; set; }
+    }
+}
