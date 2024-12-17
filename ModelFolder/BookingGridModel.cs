2aba430d403f0de8068998022ccfce0fed4a04c1
JoHB94 <webdp1503@gmail.com>
2024-12-18 00:55:03 +0900

    add BookingGridModel 추가

---
+++ ModelFolder/BookingGridModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mataju.ModelFolder
+{
+    public class BookingGridModel
+    {
+        public string Size { get; set; } // "S", "M", "L"
+        public int Price { get; set; }
+        public int AvailableCount { get; set; }
+    }
+}
